5497c7edb52ff6355f2e874b03a61aa3aed6d761
Jyoti Ranjan Jally <38002546+jyotiranjanjally@users.noreply.github.com>
2024-05-26 18:59:59 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://jyotiranjanjally.github.io/", // replace this with your deployed domain
+  author: "Jyoti Ranjan Jally",
+  desc: "Jyoti Tech Blogs",
+  title: "Jyoti Ranjan Jally",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,

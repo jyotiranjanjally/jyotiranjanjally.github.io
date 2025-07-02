d94e275ef7dac41e31ab1b2d0309e2338b4ecf85
Jyoti Ranjan Jally <38002546+jyotiranjanjally@users.noreply.github.com>
2025-07-02 14:04:51 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,11 +9,12 @@ const blog = defineCollection({
       pubDatetime: z.date(),
       modDatetime: z.date().optional().nullable(),
       title: z.string(),
+      slug: z.string().optional(), // ✅ <-- add this line
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
       tags: z.array(z.string()).default(["others"]),
       ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
